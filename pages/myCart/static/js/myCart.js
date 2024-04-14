aeeb723b3ac39b0a8b61b010dc39de14672c5024
NeriyaC <135694507+neriya-hub@users.noreply.github.com>
2024-04-14 12:05:28 +0300

    myCart.js

--- pages/myCart/static/js/myCart.js
+++ pages/myCart/static/js/myCart.js
@@ -1,71 +1,54 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const cartItemsContainer = document.querySelector('.cart-container');
-    const totalAmount = document.getElementById('total-price');
 
-    // Function to fetch cart items from the server
-    function fetchCartItems() {
-        fetch('/cart_items')
-            .then(response => response.json())
-            .then(data => {
-                // Display the fetched cart items
-                displayCartItems(data);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    const cakesData = JSON.parse(localStorage.getItem('cart')) || [];
+
+    const cartItemsContainer = document.querySelector('.cart-container');
+    const totalAmount  = document.getElementById('total-price');
+
+
+
+    // Display cart items
+    displayCartItems();
+
 
     // Function to display cart items
-    function displayCartItems(cartItems) {
+    function displayCartItems() {
+
         // Clear previous items
         cartItemsContainer.innerHTML = '';
         let totalPrice = 0;
 
         // Display cakes in the cart
-        cartItems.forEach((item, index) => {
-            item._id = undefined;
+        cakesData.forEach((cake, index) => {
             const cartItem = document.createElement('div');
             cartItem.classList.add('cart-item');
             cartItem.innerHTML = `
-                <div class="cake-name">${item.name}</div>
-                <div class="cake-price">$${item.price}</div>
-                <img src="${item.image}" alt="${item.name}" class="cake-image">
-                <button class="remove-from-cart-btn" data-id="${item._id}">Remove from Cart</button>`;
+                <div class="cake-name">${cake.name}</div>
+               <div class="cake-price">$${cake.price}</div>
+                <img src="${cake.image}" alt="${cake.name}" class="cake-image">
+                <button class="remove-from-cart-btn" data-index="${index}">Remove from Cart</button> `;
             cartItem.querySelector('.remove-from-cart-btn').addEventListener('click', function () {
-                removeFromCart(item._id);
+                removeFromCart(index);
             });
-            totalPrice += parseFloat(item.price);
+             totalPrice += parseFloat(cake.price);
             cartItemsContainer.appendChild(cartItem);
         });
 
-        // Display total price
-        totalAmount.textContent = `$${totalPrice.toFixed(2)}`;
+    // Calculate total price
+    const total = cakesData.reduce((acc, cake) => acc + parseFloat(cake.price), 0);
+
+    // Display total price
+    totalAmount.textContent = `$${total.toFixed(2)}`;
+
+    // Store the total amount in local storage
+    localStorage.setItem('totalAmount', totalPrice);
     }
+
 
     // Function to remove item from cart
-    function removeFromCart(itemId) {
-        // Send a request to the server to remove the item from the user's cart in the database
-        fetch(`/remove_from_cart/${itemId}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({}),
-        })
-            .then(response => response.json())
-            .then(data => {
-                if (data.success) {
-                    // Refresh the cart items
-                    fetchCartItems();
-                } else {
-                    console.error('Failed to remove item from cart.');
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
+    function removeFromCart(index) {
+        cakesData.splice(index, 1);
+        localStorage.setItem('cart', JSON.stringify(cakesData));
+        displayCartItems();
     }
-
-    // Initial fetch of cart items
-    fetchCartItems();
 });
