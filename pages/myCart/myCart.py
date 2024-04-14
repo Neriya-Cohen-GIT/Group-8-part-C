1a8bb30d1a0d0f03e837fb496387e8b2ee73171c
NeriyaC <135694507+neriya-hub@users.noreply.github.com>
2024-04-14 12:05:57 +0300

    myCart.py

--- pages/myCart/myCart.py
+++ pages/myCart/myCart.py
@@ -1,10 +1,7 @@
 # Import necessary modules
-from flask import Blueprint, render_template, request, jsonify, session
-from pymongo import MongoClient
+from flask import Blueprint, render_template
 
-# MongoDB connection setup
-client = MongoClient('mongodb://localhost:27017/')
-db = client['your_database']  # Replace 'your_database' with the name of your MongoDB database
+
 
 # myCart blueprint definition
 myCart = Blueprint(
@@ -23,25 +20,3 @@ def index():
     return render_template('myCart.html')
 
 
-@myCart.route('/cart_items', methods=['GET'])
-def get_cart_items():
-    # Get the current user's cart from MongoDB based on the user's session
-    user_cart = db['carts'].find_one({'user_id': session.get('user_id')})
-
-    # If the user has a cart, return it, otherwise, return an empty list
-    cart_items = user_cart['items'] if user_cart else []
-
-    return jsonify(cart_items)
-
-
-@myCart.route('/remove_from_cart/<string:item_id>', methods=['POST'])
-def remove_from_cart(item_id):
-    try:
-        # Remove the item from the user's cart in the database
-        db['carts'].update_one(
-            {'user_id': session.get('user_id')},
-            {'$pull': {'items': {'_id': item_id}}}
-        )
-        return jsonify({'success': True})
-    except Exception as e:
-        return jsonify({'success': False, 'error': str(e)})
